bd907ea6e0bae40503752faaf6c74af06a77c8b9
Musab Akan <musab.akan@hotmail.com>
2025-06-01 21:17:27 +0300

    #docker File

--- src/main/java/com/mskn/vaadinspringproject/backend/security/JpaConfiguration.java
+++ src/main/java/com/mskn/vaadinspringproject/backend/security/JpaConfiguration.java
@@ -19,7 +19,6 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.util.Properties;
 
@@ -35,25 +34,24 @@ public class JpaConfiguration {
     @Autowired
     private Environment environment;
 
-    @Value("${datasource.musakan.maxPoolSize:10}")
+    @Value("${spring.datasource.hikari.maximum-pool-size:10}")
     private int maxPoolSize;
 
     @Bean
     @Primary
-    @ConfigurationProperties(prefix = "datasource.musakan")
+    @ConfigurationProperties(prefix = "spring.datasource")
     public DataSourceProperties dataSourceProperties() {
         return new DataSourceProperties();
     }
 
     @Bean
     public DataSource dataSource() {
-        DataSourceProperties dataSourceProperties = dataSourceProperties();
-        HikariDataSource dataSource = (HikariDataSource) DataSourceBuilder
-                .create(dataSourceProperties.getClassLoader())
-                .driverClassName(dataSourceProperties.getDriverClassName())
-                .url(dataSourceProperties.getUrl())
-                .username(dataSourceProperties.getUsername())
-                .password(dataSourceProperties.getPassword())
+        DataSourceProperties dsp = dataSourceProperties();
+        HikariDataSource dataSource = DataSourceBuilder.create()
+                .driverClassName(dsp.getDriverClassName())
+                .url(dsp.getUrl())
+                .username(dsp.getUsername())
+                .password(dsp.getPassword())
                 .type(HikariDataSource.class)
                 .build();
         dataSource.setMaximumPoolSize(maxPoolSize);
@@ -61,10 +59,10 @@ public class JpaConfiguration {
     }
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws NamingException {
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
         factoryBean.setDataSource(dataSource());
-        factoryBean.setPackagesToScan(new String[]{"com.mskn.vaadinspringproject.backend.entities"});
+        factoryBean.setPackagesToScan("com.mskn.vaadinspringproject.backend.entities");
         factoryBean.setJpaVendorAdapter(jpaVendorAdapter());
         factoryBean.setJpaProperties(jpaProperties());
         return factoryBean;
@@ -72,19 +70,17 @@ public class JpaConfiguration {
 
     @Bean
     public JpaVendorAdapter jpaVendorAdapter() {
-        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
-        return hibernateJpaVendorAdapter;
+        return new HibernateJpaVendorAdapter();
     }
 
     private Properties jpaProperties() {
         Properties properties = new Properties();
-        properties.put("hibernate.dialect", environment.getRequiredProperty("datasource.musakan.hibernate.dialect"));
-        properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("datasource.musakan.hibernate.hbm2ddl.method"));
-        properties.put("hibernate.show_sql", environment.getRequiredProperty("datasource.musakan.hibernate.show_sql"));
-        properties.put("hibernate.format_sql", environment.getRequiredProperty("datasource.musakan.hibernate.format_sql"));
+        properties.put("hibernate.dialect", environment.getRequiredProperty("spring.jpa.properties.hibernate.dialect"));
+        properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("spring.jpa.hibernate.ddl-auto"));
+        properties.put("hibernate.show_sql", environment.getRequiredProperty("spring.jpa.properties.hibernate.show_sql"));
+        properties.put("hibernate.format_sql", environment.getRequiredProperty("spring.jpa.properties.hibernate.format_sql"));
         return properties;
     }
-
 
     @Bean
     public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
