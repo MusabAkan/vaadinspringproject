92594f5882d72970e2d37ada9a5e6dd20234760a
Musab Akan <musab.akan@hotmail.com>
2025-06-01 17:04:25 +0300

    #initial package

---
+++ src/test/java/com/mskn/vaadinspringproject/VaadinspringprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mskn.vaadinspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinspringprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
